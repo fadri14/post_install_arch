e27cb5bc150b5cc9ef5469dd4df424bd51936e6e
fadri14 <fadri1@proton.me>
2024-04-10 16:11:06 +0200

    Mise à jour de la config

--- myconfig/fish/config.fish
+++ myconfig/fish/config.fish
@@ -1,3 +1,14 @@
+function ssh_agent_init.fish
+    set ssh_agent_var $(keychain --eval --quiet -Q id_ed25519)
+    set ssh_agent_pid $(pgrep ssh-agent)
+    set ssh_agent_name $(echo $ssh_agent_var | cut -d "/" -f3)
+    
+    set SSH_AUTH_SOCK /tmp/$ssh_agent_name/agent.$(math $ssh_agent_pid - 1)
+    export SSH_AUTH_SOCK
+    set SSH_AGENT_PID $ssh_agent_pid
+    export SSH_AGENT_PID
+end
+
 if status is-interactive
     set -g fish_greeting ''
 
@@ -32,6 +43,8 @@ if status is-interactive
     alias gitco "eval (ssh-agent -c) > /dev/null && ssh-add  ~/.ssh/github && killall ssh-agent"
 
     bind \eh 'wtype !!\n'
+
+    ssh_agent_init.fish
 
     starship init fish | source
 end
